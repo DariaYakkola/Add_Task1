6c5e95d7dfa7841b79e27b8a197305942f32aaa0
Daria Yakkola <yakkolabest@gmail.com>
2023-02-22 12:50:41 +0300

    Дополнительная задача 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите сумму вклада: ");
+int deposit = Convert.ToInt32(Console.ReadLine());
+if (deposit > 200000)
+{
+  int sum = (deposit / 100) * 10 + deposit;
+  Console.WriteLine($"Сумма вклада вместе с начисленными процентами составит {sum} ");
+}
+else if (deposit >= 100000 && deposit <= 200000)
+{
+  int sum = (deposit / 100) * 7 + deposit;
+  Console.WriteLine($"Сумма вклада вместе с начисленными процентами составит {sum} ");
+}
+else
+{
+  int sum = (deposit / 100) * 5 + deposit;
+  Console.WriteLine($"Сумма вклада вместе с начисленными процентами составит {sum} ");
+}
